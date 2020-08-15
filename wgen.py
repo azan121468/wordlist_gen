1b302a5c605364ca0a439c8c6b0cf11f40c38905
Azan Shahid <67824275+azan121468@users.noreply.github.com>
2020-08-15 01:21:34 -0700

    Update wgen.py

--- wgen.py
+++ wgen.py
@@ -1,26 +1,37 @@
 import os
 import sys
-import string
 import itertools
 import sys
 
+total_words=0
+
 def title():
+    """display title box"""
     print("****************************")
     print("*    Wordlist Generator    *")
     print("*            By            *")
     print("*        Azan  Shahid      *")
     print("****************************")
-def remove_duplicates(str_of_chars):
-    set_of_chars=set(str_of_chars)
-    string=""  #string which functio will return
-    for i in set_of_chars:
-        string+=i
-    return string
-def combination(n,r):
-   pass
 
-def createWordList(chrs, min_length, max_length, head, tail, output):
+def remove_duplicates(string):
+    """remove duplicate words from string"""
+    string=set(string)
+    return_string=""
+    for i in string:
+        return_string+=i
+    return_string=sort_string(return_string)
+    return return_string
 
+def sort_string(string):
+    """sort characters of string"""
+    return_string=''.join(sorted(string))
+    return return_string
+
+
+
+def createWordList(chars, min_length, max_length, head, tail, output):
+    """Creates wordlist based on given data"""
+    global total_words
     if min_length > max_length:
         print ("[!] Please `min_length` must smaller or same as with `max_length`")
         sys.exit()
@@ -30,11 +41,19 @@ def createWordList(chrs, min_length, max_length, head, tail, output):
     output = open(output, 'w')
 
     try:
+        reverse_chars=[]
         for n in range(min_length, max_length + 1):
-            for i in itertools.product(chrs, repeat=n):
+            for i in itertools.product(chars, repeat=n):
                 chars = head+''.join(i)+tail
+                reverse_chars.append(chars[::-1])
                 output.write("%s\n" % chars)
                 sys.stdout.write('\r[+] saving character `%s`' % chars)
+                total_words+=1
+                sys.stdout.flush()
+            for i in reverse_chars:
+                output.write("%s\n" % i)
+                sys.stdout.write('\r[+] saving character `%s`' % i)
+                total_words += 1
                 sys.stdout.flush()
         output.close()
     except KeyboardInterrupt:
@@ -42,6 +61,7 @@ def createWordList(chrs, min_length, max_length, head, tail, output):
 
 
 def main():
+    """main part of program"""
     title()
     print("Enter characterset : ",end="")
     chars=input()
@@ -58,7 +78,8 @@ def main():
     output=input()
     try:
         createWordList(chars, min_length, max_length, head, tail, output)
+        print(f"\nNumber of words written are {total_words}")
     except PermissionError:
         print("[!] You have no permission to write here")
-        print("[?] You need administrator permission to write here")
+        print("[?] You need administrator permission to wrote here")
 main()
