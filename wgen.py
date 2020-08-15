a74d9a1dbbcfe0fedcc1ff30ed828c15e6843158
Azan Shahid <67824275+azan121468@users.noreply.github.com>
2020-08-15 00:08:50 -0700

    Update wgen.py

--- wgen.py
+++ wgen.py
@@ -1,59 +1,64 @@
 import os
 import sys
-import time
 import string
-import argparse
 import itertools
+import sys
 
+def title():
+    print("****************************")
+    print("*    Wordlist Generator    *")
+    print("*            By            *")
+    print("*        Azan  Shahid      *")
+    print("****************************")
+def remove_duplicates(str_of_chars):
+    set_of_chars=set(str_of_chars)
+    string=""  #string which functio will return
+    for i in set_of_chars:
+        string+=i
+    return string
+def combination(n,r):
+   pass
 
-def createWordList(chrs, min_length, max_length, output):
-    """
-    :param `chrs` is characters to iterate.
-    :param `min_length` is minimum length of characters.
-    :param `max_length` is maximum length of characters.
-    :param `output` is output of wordlist file.
-    """
+def createWordList(chrs, min_length, max_length, head, tail, output):
+
     if min_length > max_length:
         print ("[!] Please `min_length` must smaller or same as with `max_length`")
         sys.exit()
 
-    if os.path.exists(os.path.dirname(output)) == False:
-        os.makedirs(os.path.dirname(output))
-
     print ('[+] Creating wordlist at `%s`...' % output)
-    print ('[i] Starting time: %s' % time.strftime('%H:%M:%S'))
 
     output = open(output, 'w')
 
-    for n in range(min_length, max_length + 1):
-        for xs in itertools.product(chrs, repeat=n):
-            chars = ''.join(xs)
-            output.write("%s\n" % chars)
-            sys.stdout.write('\r[+] saving character `%s`' % chars)
-            sys.stdout.flush()
-    output.close()
+    try:
+        for n in range(min_length, max_length + 1):
+            for i in itertools.product(chrs, repeat=n):
+                chars = head+''.join(i)+tail
+                output.write("%s\n" % chars)
+                sys.stdout.write('\r[+] saving character `%s`' % chars)
+                sys.stdout.flush()
+        output.close()
+    except KeyboardInterrupt:
+        print("\nUser has exited using CTRL + C")
 
-    print ('\n[i] End time: %s' % time.strftime('%H:%M:%S'))
 
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawTextHelpFormatter,
-        description='Python Wordlist Generator')
-    parser.add_argument(
-        '-chr', '--chars',
-        default=None, help='characters to iterate')
-    parser.add_argument(
-        '-min', '--min_length', type=int,
-        default=1, help='minimum length of characters')
-    parser.add_argument(
-        '-max', '--max_length', type=int,
-        default=2, help='maximum length of characters')
-    parser.add_argument(
-        '-out', '--output',
-        default='output/wordlist.txt', help='output of wordlist file.')
-
-    args = parser.parse_args()
-    if args.chars is None:
-        args.chars = string.printable.replace(' \t\n\r\x0b\x0c', '')
-    createWordList(args.chars, args.min_length, args.max_length, args.output)
+def main():
+    title()
+    print("Enter characterset : ",end="")
+    chars=input()
+    chars=remove_duplicates(chars)
+    print("Enter minimum length : ",end="")
+    min_length=int(input())
+    print("Enter maximum length : ", end="")
+    max_length = int(input())
+    print("Head (if any) : ",end="")
+    head=input() or ""
+    print("Tail (if any) : ",end="")
+    tail=input() or ""
+    print("Enter output : ",end="")
+    output=input()
+    try:
+        createWordList(chars, min_length, max_length, head, tail, output)
+    except PermissionError:
+        print("[!] You have no permission to write here")
+        print("[?] You need administrator permission to wrote here")
+main()
