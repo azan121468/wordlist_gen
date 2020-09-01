d32f85c50bc7c65ad215b8885c0ba30a6270bab7
Azan Shahid <67824275+azan121468@users.noreply.github.com>
2020-09-01 00:50:28 -0700

    Update and rename wgen.py to small_wgen.py

--- small_wgen.py
+++ small_wgen.py
@@ -41,21 +41,42 @@ def createWordList(chars, min_length, max_length, head, tail, output):
     output = open(output, 'w')
     try:
         strings_to_write=[]
+        reverse_of_strings_to_write=[]
         print("[+] Saving strings in memory")
-        for n in range(min_length, max_length + 1):
-            for i in itertools.product(chars, repeat=n):
-                word = head+''.join(i)+tail
-                reverse_word = head + ''.join(i)[::-1] + tail
-                strings_to_write.append(word)
-                strings_to_write.append(reverse_word)
+        try:
+            for n in range(min_length, max_length + 1):
+                for i in itertools.product(chars, repeat=n):
+                   word = head+''.join(i)+tail
+                   reverse_word = head + ''.join(i)[::-1] + tail
+                   strings_to_write.append(word)
+                   reverse_of_strings_to_write.append(reverse_word)
+        except MemoryError:
+            print("[-] Memory Error has been occured")
+            print("[-] Use big_wgen.py to generate wordlist")
+            print("[+] Removing list from memory")
+            del strings_to_write
+            print("[+] Sucessfully deleted list from memory")
+            sys.exit()
+        except :
+            print("[-] User has interrupt while chraracter were being saved in memory.")
+            print("[+] Removing list from memory")
+            del strings_to_write
+            print("[+] Sucessfully deleted list from memory")
+            sys.exit()
         print("[+] Removing duplicates if any")
         strings_to_write = list(set(strings_to_write))
+        reverse_of_strings_to_write = list(set(reverse_of_strings_to_write))
         print("[+] Duplicates removed sucessfully")
         print("[+] Write in memory sucessful")
         print("[+] Writing strings on disk")
         for i in strings_to_write:
             output.write("%s\n" % i)
             sys.stdout.write('\r[+] saving character `%s`' % i)
+            total_words += 1
+            sys.stdout.flush()
+        for m in reverse_of_strings_to_write:
+            output.write("%s\n" % m)
+            sys.stdout.write('\r[+] saving character `%s`' % m)
             total_words += 1
             sys.stdout.flush()
         print("\n[+] Write in disk sucessful")
@@ -89,6 +110,6 @@ def main():
     except PermissionError:
         print("[!] You have no permission to write here")
         print("[?] You need administrator permission to wrote here")
-        
+
 if __name__=="__main__":
     main()
